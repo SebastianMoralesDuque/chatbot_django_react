510960de7b57dbd6feec36f87e655c123aa340fb
Sebastian <cheviotin200@gmail.com>
2023-05-22 00:12:23 -0500

    urls for add data and process response

--- chatbot-server/chat/urls.py
+++ chatbot-server/chat/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import send_message
+from .views import send_message, process_data, add_qa_pairs
 
 app_name = 'chat'
 
 urlpatterns = [
     path('send-message/', send_message, name='send_message'),
+    path('process-data/', process_data, name='process_data'),
+    path('add-qa-pairs/', add_qa_pairs, name='add_qa_pairs'),
 ]
